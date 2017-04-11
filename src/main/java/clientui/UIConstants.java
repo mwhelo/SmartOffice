dddf11531ed7106e86060921393ff765e715de79
Matthew Whelan <matthew.whelan@student.ncirl.ie>
2017-04-11 11:54:39 +0100

    First Commit

---
+++ src/main/java/clientui/UIConstants.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientui;
+
+/**
+ *
+ * @author x12431142
+ */
+public class UIConstants {
+    
+    public static final int UIWIDTH = 550;
+    public static final int UIHEIGHT = 600;
+    public static final int COMPONENTWIDTH = 520;
+    public static final int CONTROLY = 425;
+    
+}
