ebb99f7973f7f7066d62a971c3dded5224c14794
Matthew Whelan <matthew.whelan@student.ncirl.ie>
2017-04-18 17:47:18 +0100

    MonitorClient added by Ryan

---
+++ src/main/java/client/MonitorClient.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import clientui.MonitorUI;
+
+/**
+ *
+ * @author Ryan
+ */
+public class MonitorClient extends Client{
+    
+    private final String on = "On";
+    private final String up = "Up";
+    private final String down = "Down";
+    private boolean isOn = false;
+    private boolean isUp = false;
+    private boolean isDown = false;
+    
+    public MonitorClient(){
+        super();
+        serviceType = "_monitor._upd.local.";
+        ui = new MonitorUI(this);
+        name = "monitor";
+    }
+    
+    public void on(){
+        if (!isOn){
+            String a = sendMessage(on);
+            if(a.equals(OK)){
+                isOn = true;
+                ui.updateArea("Monitor turned on");
+            }
+        } else {
+            ui.updateArea("Monitor already running");
+        }
+    }
+    
+    public void channelUp(){
+        if(!isUp){
+            String a = sendMessage(up);
+            if(a.equals(OK)){
+                isUp = true;
+                ui.updateArea("Channel changed up..");
+            }
+        }
+    }
+    
+    public void channelDown(){
+        if(!isDown){
+            String a = sendMessage(down);
+            if(a.equals(OK)){
+                isDown = true;
+                ui.updateArea("Channel changed down..");
+            }
+        }
+    }
+    
+    @Override
+    public void updatePoll(String msg){
+        if (msg.equals("Monitor turned on")){
+            isOn = true;
+        }
+        else if(msg.equals("Channel changed up..")){
+            isUp = true;
+        }
+        else if(msg.equals("Channel changed down..")){
+            isDown = true;
+        }
+    }
+    
+    @Override
+    public void disable(){
+        super.disable();
+        ui = new MonitorUI(this);
+        isOn = false;
+        isUp = false;
+        isDown = false;
+    }
+    
+}
